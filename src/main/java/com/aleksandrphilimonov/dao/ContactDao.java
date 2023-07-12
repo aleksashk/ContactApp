8edba5418438d305132d040c343ebfc51d45a2a0
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-12 14:10:16 +0300

    add HikariDataSource

--- src/main/java/com/aleksandrphilimonov/dao/ContactDao.java
+++ src/main/java/com/aleksandrphilimonov/dao/ContactDao.java
@@ -2,12 +2,45 @@ package com.aleksandrphilimonov.dao;
 
 import com.aleksandrphilimonov.dao.domain.Contact;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import static com.aleksandrphilimonov.dao.DaoFactory.getConnection;
+
 public class ContactDao implements DAO<Contact, Integer> {
+
+    private static ContactDao contactDao;
+
+    private ContactDao() {
+    }
+
+    public static ContactDao getContactDao() {
+        if (contactDao == null) {
+            contactDao = new ContactDao();
+        }
+        return contactDao;
+    }
+
     @Override
-    public Contact findById(Integer integer) {
-        return null;
+    public Contact findById(Integer id) throws SQLException {
+        Contact contact = null;
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement ps = connection.prepareStatement("select * from contact where id = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                contact = new Contact();
+                contact.setId(rs.getInt("id"));
+                contact.setFirstName(rs.getString("first_name"));
+                contact.setLastName(rs.getString("last_name"));
+            }
+        }
+
+        return contact;
     }
 
     @Override
--- src/main/java/com/aleksandrphilimonov/dao/DAO.java
+++ src/main/java/com/aleksandrphilimonov/dao/DAO.java
@@ -1,9 +1,10 @@
 package com.aleksandrphilimonov.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO<DOMAIN, ID> {
-    DOMAIN findById(ID id);
+    DOMAIN findById(ID id) throws SQLException;
 
     List<DOMAIN> findByAll();
 
