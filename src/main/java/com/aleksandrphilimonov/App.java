7e25ec8f1a800a7edab966e36dd8f4e440509490
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-12 12:27:02 +0300

    Initial commit

---
+++ src/main/java/com/aleksandrphilimonov/App.java
@@ -0,0 +1,13 @@
+package com.aleksandrphilimonov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
