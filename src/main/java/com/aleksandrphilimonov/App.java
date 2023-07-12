f433c732304d30de254e6661d976774905aa5c16
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-12 12:52:15 +0300

    PreparedStatement

--- src/main/java/com/aleksandrphilimonov/App.java
+++ src/main/java/com/aleksandrphilimonov/App.java
@@ -1,7 +1,9 @@
 package com.aleksandrphilimonov;
 
+import java.lang.ref.PhantomReference;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,20 +13,23 @@ import java.sql.Statement;
  */
 public class App {
     public static void main(String[] args) {
+        Long contactId = 1L;
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/postgres_db",
                 "postgres",
                 "password"
         )) {
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(
-                    "select " +
-                            "c.first_name," +
-                            "c.last_name," +
-                            "count(pn.phone_number)" +
-                            "from contact as c " +
-                            "left join phone_number pn on pn.contact_id = c.id " +
-                            "group by c.first_name, c.last_name;");
+            PreparedStatement statement = connection.prepareStatement("select " +
+                    "c.id," +
+                    "c.first_name," +
+                    "c.last_name," +
+                    "count(pn.phone_number)" +
+                    "from contact as c " +
+                    "left join phone_number pn on pn.contact_id = c.id " +
+                    "where c.id=?" +
+                    "group by c.id, c.first_name, c.last_name;");
+            statement.setLong(1, contactId);
+            ResultSet rs = statement.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getString("first_name") + "\t" +
                         rs.getString("last_name") + "\t" +
