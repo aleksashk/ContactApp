05699a99940feba59826783aaff15eaea071cdb7
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-14 18:07:10 +0300

    H2, Liquibase refactoring

--- src/main/java/com/aleksandrphilimonov/App.java
+++ src/main/java/com/aleksandrphilimonov/App.java
@@ -1,11 +1,8 @@
 package com.aleksandrphilimonov;
 
 import com.aleksandrphilimonov.exception.CommonServiceException;
-import com.aleksandrphilimonov.service.ServiceFactory;
 
 public class App {
     public static void main(String[] args) throws CommonServiceException {
-        SecurityService securityService = ServiceFactory.getSecurityService();
-        System.out.println(securityService.auth("aleks@gmail.com", "password"));
     }
 }
